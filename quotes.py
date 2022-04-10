170b180fedc43d21960fcc6d66bd9a9e8342703c
andrew5255 <93953687+andrew5255@users.noreply.github.com>
2022-04-10 03:54:04 -0500

    Add files via upload

--- quotes.py
+++ quotes.py
@@ -1,17 +1,11 @@
+# Pre-made quotes list and function that returns a random quote
 import random as ran
 
 quoteslist = ["You are loved.", "Call your mom.", "You are beautiful", "You can get through any problem.",
-              "You are awesome.", "Yo mama you looking good asl today"]
+              "You are awesome.", "Yo mama, you looking good asl today", "If it makes you feel better, Thaddeaus is "
+                                                                         "uglier than you."]
 
 
 def giveRandomQuote():
-    num = ran.randrange(0, 5)
+    num = ran.randrange(0, len(quoteslist))
     print(quoteslist[num])
-
-
-def main():
-    giveRandomQuote()
-
-
-if __name__ == "__main__":
-    main()
