5552376a205ab335b3d57eb225d302497e4d95d7
04Isaias <isaias.r0423@gmail.com>
2022-04-15 15:49:10 -0500

    Add files via upload

--- Video_capture.py
+++ Video_capture.py
@@ -3,23 +3,23 @@
 #
 #
 
-import sensor, image, pyb, time, machine
+import sensor, image, pyb, time, machine, random
 from pyb import UART
 
 uart = UART(3, 115200, timeout_char=1000)
-uart.write('Program Started.\n\r')
+
 boolean = 0
 
 fileName = "/Memory"
 vidNum = 0
-
-record_time = 1000 # 1 seconds in milliseconds
+record_time = 120000 # 5 seconds in milliseconds (2min)
 
 while 1:
     pyb.LED(3).off()
     boolean = uart.read() #reads from terminal
 
     if boolean: #when user enters 1 in terminal, begin recording for 10 seconds
+        uart.write('Program Started.\n\r')
         sensor.reset()
         sensor.set_pixformat(sensor.RGB565)
         sensor.set_framesize(sensor.QVGA)
@@ -47,16 +47,18 @@ while 1:
         pyb.LED(3).on()
 
         message = fileName + ' recorded as a file onto the SD card\n\r'
-        uart.write(message)
+        #uart.write(message)
 
         vidNum = vidNum + 1
         fileName = "/Memory" # reseting the name
+        uart.write('\n\r')
     Boolean = 0 #reseting the flag to wait for another response from app
 
-    if vidNum == 2:
-        break
-uart.write('All videos recorded, thank you for using CAPTURE')
-machine.reset()
+    #if vidNum == 1:
+    #    break
+#uart.write('\n\r')
+
+#machine.reset()
 
 #Convert file from .bin to readable .mp4 file and transfer it back to terminal to be downloaded
 #We're unsure if this can be done via UART. If not, what other ways make this possible?
