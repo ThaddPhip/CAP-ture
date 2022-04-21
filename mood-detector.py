8f16e5404fe370966062eed7febf8cc5d4dd8a94
ThaddPhip <104166220+ThaddPhip@users.noreply.github.com>
2022-04-21 16:56:16 -0500

    Update mood-detector.py

--- mood-detector.py
+++ mood-detector.py
@@ -5,17 +5,24 @@
 #then make this a main file for the emotional assessment of any data from that person in the future
 #by calling the results from this file
 #when the main file determines a mood save that indication to a separate file in python that displays/saves vids and quotes
+
+
+#WhichCSV = input("CSV File Name: ")
+
 #code for threshold calc:
 from csv import reader
 # open file in read mode
+items = [] #Array to be used
 with open('thaddeaus_happy.csv', 'r') as read_obj:
     # pass the file object to reader() to get the reader object
     csv_reader = reader(read_obj)
     # Iterate over each row in the csv using reader object
     for row in csv_reader:
         # row variable is a list that represents a row in csv
-        print(row)
-
+        items.append(row[0]) #Apparently saves each row to an array
+        #print(row)
+print(items) #Printing each row in the array
+      
 #find columns we need to assess
 #Cut off first x secs (rows) and last x secs (rows)- you can also do this in for loop like for col=x:col=x
 #filter out the blinks w a logic statement of if there is a value in the Beta_TP10 column
@@ -26,6 +33,37 @@ with open('thaddeaus_happy.csv', 'r') as read_obj:
 
 
 #then for each new patient we do clinical trial of diff moods and find their thresholds to use for future data
+
+
+#Extracting columns from CSV File without Pandas | https://codereview.stackexchange.com/questions/252441/python-script-to-extract-columns-from-a-csv-file
+#import csv
+
+#filename = 'thaddeaus_happy.csv'
+
+#with open(filename, 'r') as csvfile:
+#    reader = csv.reader(csvfile)
+#    fields = next(reader) # Reads header row as a list
+#    rows = list(reader)   # Reads all subsequent rows as a list of lists
+
+#for column_number, field in enumerate(fields): # (0, routers), (1, servers) - Thadd: "Routers & servers were the name of his headers in his list. So ours would be 1, Delta_TP9"
+#    print(field)
+#    for row in rows:
+#        print(row[column_number])
+#    print('\n')
+
+
+
+
+
+#Filtering List In CSV File | https://www.codegrepper.com/code-examples/python/read+specific+rows+from+csv+in+python
+#You could use a list comprehension to filter the file like so:
+#with open('thaddeaus_happy.csv') as fd:
+#    reader=csv.reader(fd)
+#    interestingrows=[row for idx, row in enumerate(reader) if idx in (28,62)]
+# now interestingrows contains the 28th and the 62th row after the header
+
+
+
 
 
 #extra code
